54c3ebf66dec92e5c1c9b5fae781a94d936a8610
Eduardo Ignacio Robles Sosa <eduardo_robles14@hotmail.com>
2024-02-13 03:23:41 -0600

    removing interface xd

--- arrays/Easy.java
+++ arrays/Easy.java
@@ -1,10 +1,63 @@
-package arrays; 
+package arrays;
+import java.util.Map; 
+import java.util.HashMap; 
+
+public class Easy {     
+    public static int[] twoSumBruteForce(int[] nums, int target) { 
+             for(int i = 0; i < nums.length; i++) {
+                for (int j = 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) 
+                    return new int[]{i, j}; 
+                }
+             }
+             return null;            
+    }
+
+    public static int[] twoSumOptimal(int[] nums, int target) {
+        // create empty key, value like data structure; using hashmap
+        Map<Integer, Integer> integerMap = new HashMap<>(); 
+        for(int  i=0; i < nums.length; i++) {
+            int complement = target - nums[i]; 
+            if (integerMap.containsKey(complement)) 
+                return new int[]{i, integerMap.get(complement)}; 
+                else
+                    integerMap.put(nums[i], i);
+        }
+        return null; 
+    }
+
+    public static int bestTimeToBuySellStock(int[] prices) {
+        int max_profit = 0; 
+        int profit = 0; 
+        int l = 0; 
+        int r = l + 1; 
+        // setting left and right pointer accordingly
+        for (int x = 0; x < prices.length - 1; x++) {            
+             if  (prices[r] > prices[l]) {
+                  profit = prices[r] - prices[l];
+                  if (profit > max_profit) 
+                    max_profit = profit;                    
+                }
+            else {
+                l = r;            
+            }
+            r++;                                 
+        }
+        return max_profit;               
+    }
 
 
+    public static boolean containsDupicate(int[] nums) {    
+        HashMap<Integer,Integer> numbers = new HashMap<>(); 
+        for(int i = 0; i < nums.length; i++) {
+            if(numbers.containsKey(nums[i]))
+                return true;  
+            else 
+                numbers.put(nums[i], i);             
+        }
 
-public Inteface Easy {
-    public static int[] twoSumBruteForce(int[] nums, int target);
-    public static int[] twoSumOptimal(int[] nums, int target); 
-    public static int bestTimeToBuySellStock(int[] prices); 
-    public static boolean containsDupicate(int[] nums);
+        return false; 
+      
+    }
+
 }
