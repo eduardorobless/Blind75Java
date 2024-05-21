7ffa43c3dbb1c5c007e9ba962f4e4d7eb76ecdd4
Eduardo Ignacio Robles Sosa <eduardo_robles14@hotmail.com>
2024-05-21 16:56:25 -0600

    solved grouping anagrams without sorting

--- string/Medium.java
+++ string/Medium.java
@@ -25,7 +25,7 @@ public class Medium {
     
 
 
-    private static int longestSubstringWithoutRepeatingCharactersSetup(String s) {
+    private static int longestSubstringWithoutRepeatingCharactersFun(String s) {
         if (s == null) return 0; 
         int maxLength = 0;
         Map<Character, Integer> lastFound = new HashMap<>();
@@ -54,7 +54,7 @@ public class Medium {
     public static int longestSubstringWithoutRepeatingCharacters() {
         String a = "abcabcbb";
         
-        return longestSubstringWithoutRepeatingCharactersSetup(a);
+        return longestSubstringWithoutRepeatingCharactersFun(a);
 
     }
 
@@ -63,8 +63,107 @@ public class Medium {
     }
 
 
-    private static List<List<String>> groupAnagrams() {
-        return null;
+
+
+    /**
+     * Group anagrams: 
+     * 
+     * 
+     * Stategy wihtout sorting:
+     * 
+     * 
+     * 1 - determine frequency of each string in the list
+     * 2 - Add frequency string to hash map as key, and add string to value
+     * 
+     * Time Complexity: 
+     * 
+     * 
+     * O(N * k)
+     * 
+     * Space Complexity:
+     * 
+     * 
+     * O(N)
+     * 
+     * 
+     * When to use it: 
+     * 
+     *  When k is much larger than N
+     * 
+     * 
+     * 
+     * Strategy with sorting: 
+     * 
+     * 
+     * 1 - Sort strings, if they are anagram they would be at the same list
+     * 
+     * Time complexity:
+     * 
+     * Space complexity:
+     * 
+     * 
+     * 
+     * When to use it 
+     * 
+     */
+    
+    
+
+
+
+
+
+
+
+
+    
+    private static String  stringFrequency(String s) {        
+        int [] freq = new int[26];
+
+        // init freq array
+        for(char myChar: s.toCharArray()) 
+            freq[myChar - 'a'] ++;
+
+        // retrieve sting 
+        char myChar = 'a';
+        StringBuilder frequencyString = new StringBuilder();
+
+        for(int alpha: freq) { 
+            frequencyString.append(myChar);
+            frequencyString.append(alpha);
+            myChar++;
+        }
+
+        return frequencyString.toString();
+
+    }
+    private static List<List<String>> groupAnagramsFun(String[] strs) {
+        Map<String, List<String> > freq = new HashMap<>(); 
+
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+
+
+        for(String s: strs) {
+            // get frequency of the string as string frequency value, if present add it to map list 
+
+            String frequencyString = stringFrequency(s); 
+            // if not create list and add it
+
+            if (freq.containsKey(frequencyString)) 
+                freq.get(frequencyString).add(s);
+            
+             else {
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                freq.put(frequencyString, list);
+            }
+        }
+
+        return new ArrayList<>(freq.values());     
+    }
+    public static List<List<String>> groupAnagrams() {
+        String [] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        return groupAnagramsFun(strs);
     }
 
     private static String longestPalindromicSubstring() {
