79617e609d00a808c93fbe7ec615e7ee12a380a3
Eduardo Ignacio Robles Sosa <eduardo_robles14@hotmail.com>
2024-06-13 01:43:41 -0600

    rotate image

--- matrix/Medium.java
+++ matrix/Medium.java
@@ -166,14 +166,47 @@ public class Medium {
      *  Objective
      *      Rotate a image matrix nxn in place, by 90 degrees (clockwise). 
      *  Strategy
+     *      Get matrix transpose
+     *      Reverse each row
      *      ...
      *  Time Complexity
-     *  Space Complexity
+     *  Space Complexity 
      * 
      * 
      *      
      */
-    private static void rotate(int[][] matrix) {        
+    private static void printMatrix(int[][] matrix) {
+        System.out.println("-----------------------------");
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void transposeMatrix(int[][] matrix) {
+        // transposing a matrix  (chaging rows  for col and changing cols for rows) 
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = i + 1; j < matrix[i].length; j++) {
+                int temp = matrix[i][j]; 
+                matrix[i][j] = matrix[j][i];  // store each row into col
+                matrix[j][i] = temp;    // store each row into col
+            }
+        }
+    }
+    private static void rotateFun(int[][] matrix) {        
+        // reverse rows
+        transposeMatrix(matrix);
+        int colLength = matrix[0].length;
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < colLength / 2 ; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][ colLength - 1 - j]; 
+                matrix[i][colLength - 1 - j] = temp;
+            }
+        }
+        
     }
 
     public static void rotate() {
@@ -182,6 +215,8 @@ public class Medium {
             {4, 5, 6}, 
             {7, 8, 9}
         };
-        rotate(matrix);
+
+        rotateFun(matrix);
+        printMatrix(matrix);
     }
 }
